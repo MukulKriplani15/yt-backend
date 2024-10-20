997547f786f9cc90902b0d9fca667fe41b7cf6b5
Mukul Kriplani <mukulkriplani09@gmail.com>
2024-10-20 17:48:13 +0530

    fixed bugs during login

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -72,11 +72,11 @@ userSchema.methods.generateAccessToken = function(){
     },
 process.env.ACCESS_TOKEN_SECRET,
  {
-    expiresIn: process.env.ACESS_TOKEN_EXPIRY
+    expiresIn: process.env.ACCESS_TOKEN_EXPIRY
  }
 )
 }
-userSchema.methods.generateAccessToken = function(){
+userSchema.methods.generateRefreshToken = function(){
     return jwt.sign({
         _id: this._id
     },
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,8 @@
 import {Router} from 'express';
-import { registerUser } from '../controllers/user.controller.js';
+import { registerUser , loginUser, logoutUser } from '../controllers/user.controller.js';
 
 import {upload} from "../middlewares/multer.middleware.js"
+import { verifyJWT } from '../middlewares/auth.middleware.js';
 const router = Router();
 
 router.route("/register").post(upload.fields(
@@ -17,4 +18,13 @@ router.route("/register").post(upload.fields(
     ]
 ),
     registerUser)
+
+    router.route("/login").post(loginUser)
+
+    // secured routed 
+
+    router.route("/logout").post(verifyJWT, logoutUser)
+
+
+
 export default router;
